3571bbf8557121499d823fa6899ae24df2ec2086
Nick Colomatin <ncolomatin@gmail.com>
2019-02-23 09:46:09 +0100

    added files

---
+++ app.js
@@ -0,0 +1,8 @@
+// function handleResponse(response) {
+//   for (var i = 0; i < response.items.length; i++) {
+//     var item = response.items[i];
+//     // in production code, item.text should have the HTML entities escaped.
+//     document.getElementById("content").innerHTML +=
+//       "<br>" + item.volumeInfo.title;
+//   }
+// }
