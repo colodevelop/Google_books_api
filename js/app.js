ecb0f39dafa3b8d07fba60f30e40515828687d20
Nick Colomatin <ncolomatin@gmail.com>
2019-02-24 12:14:36 +0100

    added the refresh button

--- js/app.js
+++ js/app.js
@@ -8,8 +8,12 @@ document.addEventListener(
       search: document.getElementById("search-books"),
       result: document.getElementById("result"),
       button: document.getElementById("search-button"),
+      refresh: document.getElementById("refresh-button"),
       message: document.querySelector(".nothing-message")
     };
+
+    // Your API key down (Currently N.Colomatin API Key)
+    var APIkey = `AIzaSyCoB6R2r7zWFZx_C50_OpL2lTvO0F2oMdI`;
 
     // Function that gets the results of the books
     function getResults() {
@@ -31,20 +35,21 @@ document.addEventListener(
         // 2. Configure it: GET-request for the URL
         xhr.open(
           "GET",
-          `https://www.googleapis.com/books/v1/volumes?q=${searchValue}`
+          `https://www.googleapis.com/books/v1/volumes?q=${searchValue}&key=${APIkey}`,
+          true
         );
 
         // 3. Send the request
         xhr.send();
 
         // 4. This will be called after the response is received
-        xhr.onload = function(response) {
+        xhr.onload = function addItems(response) {
           let data = JSON.parse(this.response);
           for (var i = 0; i < 11; i++) {
             // DOM element of ALL books.
-            var allBooks = document.querySelector(".all-books");
-            //Creates the elements for the Book-card
+            allBooks = document.querySelector(".all-books");
 
+            //Creates the elements for the Book-card
             var divCard = document.createElement("div");
             var imageCard = document.createElement("img");
             var textCard = document.createElement("div");
@@ -84,6 +89,13 @@ document.addEventListener(
       }
     }
 
+    function refreshBooks() {
+      allBooks.innerHTML = "";
+    }
+
+    // document.removeChild(allBooks);
+    //no code yet..
+
     // When clicked on button, will fire the getResults function
     DOM.button.addEventListener("click", getResults);
 
@@ -92,6 +104,8 @@ document.addEventListener(
         getResults();
       }
     });
+
+    DOM.refresh.addEventListener("click", refreshBooks);
   },
   false
 );
